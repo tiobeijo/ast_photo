ff0b45be29b07bfd0c5f4192d9b1264bfb657438
DavidLordCaos <davidlucas807@yahoo.com.br>
2019-11-28 11:45:05 -0300

    added test in photo field to upload an image

--- cypress/integration/Tests/testing_content_type_alunos.spec.js
+++ cypress/integration/Tests/testing_content_type_alunos.spec.js
@@ -12,6 +12,16 @@ describe('Ast_photo login test', function() {
         cy.contains('Alunos').click()
         cy.get('input[name="field_name[0][value]"]').type('Peter Junior', {force:true})
         cy.contains('Tirar foto com a Webcam')
+
+        const galleryPath = 'images/image1.jpg';
+
+      cy.fixture(galleryPath).then(contentGallery => {
+        cy.get('[name="files[field_profile_picture_0]"]')
+        .upload([
+          {fileContent: contentGallery, fileName: 'image1.jpg', mimeType: 'image/JPEG' },
+          ]);
+      });
+      cy.wait(2000)
         cy.get('input[name="field_age[0][value]"]').type('17')
         cy.get('input[name="field_address[0][value]"]').type('Rua Churchill, 48, Winston')
         cy.contains('Salvar').click()
