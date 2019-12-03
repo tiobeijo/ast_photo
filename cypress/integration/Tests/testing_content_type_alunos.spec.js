186655b3eef47f755dec97d56736d98ddb39373f
DavidLordCaos <davidlucas807@yahoo.com.br>
2019-12-03 13:46:51 -0300

    Added more lines to take the three student registration test and to check the alphabetical ordering and student photo appearance.

--- cypress/integration/Tests/testing_content_type_alunos.spec.js
+++ cypress/integration/Tests/testing_content_type_alunos.spec.js
@@ -11,7 +11,7 @@ describe('Ast_photo login test', function() {
         cy.contains('Adicionar conteúdo').click()
         cy.contains('Alunos').click()
         cy.get('input[name="title[0][value]"]').type('Juno Zireael', {force:true})
-        cy.get('input[name="field_name[0][value]"]').type('Juno Zireael', {force:true})
+        cy.get('input[name="field_name[0][value]"]').type('Juno Zireael da Costa Souza', {force:true})
         cy.contains('Tirar foto com a Webcam')
 
         const galleryPath = 'images/image1.jpg';
@@ -22,9 +22,58 @@ describe('Ast_photo login test', function() {
           {fileContent: contentGallery, fileName: 'image1.jpg', mimeType: 'image/JPEG' },
           ]);
       });
-      cy.wait(2000)
+      cy.wait(1000)
         cy.get('input[name="field_age[0][value]"]').type('17')
-        cy.get('input[name="field_address[0][value]"]').type('Rua Churchill, 48, Winston')
+        cy.get('input[name="field_address[0][value]"]').type('Rua Yen Merigold, 23, Ciri')
         cy.contains('Salvar').click()
+
+
+
+  
+          cy.contains('Adicionar conteúdo').click()
+          cy.contains('Alunos').click()
+          cy.get('input[name="title[0][value]"]').type('Aloy Aurora', {force:true})
+          cy.get('input[name="field_name[0][value]"]').type('Aloy Aurora da Costa Souza', {force:true})
+          cy.contains('Tirar foto com a Webcam')
+  
+          const imagePath = 'images/image2.jpg';
+  
+        cy.fixture(imagePath).then(contentGallery => {
+          cy.get('[name="files[field_profile_picture_0]"]')
+          .upload([
+            {fileContent: contentGallery, fileName: 'image2.jpg', mimeType: 'image/JPEG' },
+            ]);
+        });
+        cy.wait(1000)
+          cy.get('input[name="field_age[0][value]"]').type('15')
+          cy.get('input[name="field_address[0][value]"]').type('Rua Yen Merigold, 23, Ciri')
+          cy.contains('Salvar').click()
+
+
+
+
+          cy.contains('Adicionar conteúdo').click()
+          cy.contains('Alunos').click()
+          cy.get('input[name="title[0][value]"]').type('Ellie Frazer', {force:true})
+          cy.get('input[name="field_name[0][value]"]').type('Ellie Frazer da Costa Souza', {force:true})
+          cy.contains('Tirar foto com a Webcam')
+  
+          const photoPath = 'images/image3.jpg';
+  
+        cy.fixture(photoPath).then(contentGallery => {
+          cy.get('[name="files[field_profile_picture_0]"]')
+          .upload([
+            {fileContent: contentGallery, fileName: 'image3.jpg', mimeType: 'image/JPEG' },
+            ]);
+        });
+        cy.wait(1000)
+          cy.get('input[name="field_age[0][value]"]').type('15')
+          cy.get('input[name="field_address[0][value]"]').type('Rua Yen Merigold, 23, Ciri')
+          cy.contains('Salvar').click()
+
+          cy.wait(2000)
+
+          cy.contains('Início').click({force:true})
+
     })
 })
